0ee9ad9234b44c302c992cae7dd038d8c4510bf0
Nikita Karpukhin <gray@graynk.space>
2025-01-05 00:47:19 +0100

    honestly incredibly awful (but working) map prototype.

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
@@ -14,6 +15,14 @@ public class Plugin : BaseUnityPlugin
   private const string InputMethodName = "OnPuzzleViewSelectInput";
   private static ManualLogSource _logger;
   private static Harmony _hi;
+  private static bool customButtonPressed = false;
+  private static bool upButtonPressed = false;
+  private static bool downButtonPressed = false;
+  private static string currentMap = "";
+  private static bool doOnce = false;
+  private static bool upOnce = false;
+  private static bool downOnce = false;
+  private static readonly List<Interact> Maptubes = new(5);
 
   private void Awake()
   {
@@ -26,11 +35,229 @@ public class Plugin : BaseUnityPlugin
   {
     _hi?.UnpatchSelf();
   }
+  
+  [HarmonyPatch(typeof(Lorelei), "Init")]
+  [HarmonyPostfix]
+  private static void StealMaptubesRefsIfAbsent()
+  {
+    // it _should_ be only 5, but just as a failsafe let's assume something went wrong and
+    // there are actually more refs for some reason - still won't stop the logic from working
+    if (Maptubes.Count >= 5)
+    {
+      return;
+    }
+    
+    var allInteractsInGame = FindObjectsByType<Interact>(FindObjectsSortMode.None);
+    foreach (var interact in allInteractsInGame)
+    {
+      if (interact.name.StartsWith("Maptube") && interact.name.EndsWith("_Interact"))
+      {
+        _logger.LogInfo($"{interact.name}");
+        Maptubes.Add(interact);
+      }
+    }
+  }
+  
+  [HarmonyPatch(typeof(LoreleiInput), "ReadInputState")]
+  [HarmonyPrefix]
+  private static bool ReadInput()
+  {
+    SGGameInput gameInput = SGFW.GameInput;
+    if (currentMap != "" && gameInput.GetButton("Interact"))
+    {
+      currentMap = "";
+    }
+    if (gameInput.GetButton("Map"))
+    {
+      customButtonPressed = true;
+    }
+    else
+    {
+      customButtonPressed = false;
+      doOnce = false;
+    }
+    if (gameInput.GetButton("Dialog_Up"))
+    {
+      upButtonPressed = true;
+    }
+    else
+    {
+      upButtonPressed = false;
+      upOnce = false;
+    }
+    if (gameInput.GetButton("Dialog_Down"))
+    {
+      downButtonPressed = true;
+    }
+    else
+    {
+      downButtonPressed = false;
+      downOnce = false;
+    }
+    if (gameInput.GetButton("Back"))
+    {
+      _logger.LogInfo($"Back is true, interact is {gameInput.GetButton("Interact")}");
+      doOnce = false;
+    }
+    return true;
+  }
+
+  private static string GetAdjacentFloorName(string currentFloor, int offset)
+  {
+    _logger.LogInfo($"GetAdjacentFloorName({currentFloor}, {offset})");
+    if (currentFloor == "Cellar")
+    {
+      if (offset == 0)
+      {
+        return "Cellar";
+      }
+
+      if (offset == 1)
+      {
+        return "Floor01";
+      }
+    }
+    if (currentFloor == "Floor01")
+    {
+      if (offset == 0)
+      {
+        return "Floor01";
+      }
+
+      if (offset == 1)
+      {
+        return "Floor02";
+      }
+
+      if (offset == -1)
+      {
+        return "Cellar";
+      }
+    }
+    if (currentFloor == "Floor02")
+    {
+      if (offset == 0)
+      {
+        return "Floor02";
+      }
+
+      if (offset == 1)
+      {
+        return "Floor03";
+      }
+
+      if (offset == -1)
+      {
+        return "Floor01";
+      }
+    }
+    if (currentFloor == "Floor03")
+    {
+      if (offset == 0)
+      {
+        return "Floor03";
+      }
+
+      if (offset == 1)
+      {
+        return "Loft";
+      }
+
+      if (offset == -1)
+      {
+        return "Floor02";
+      }
+    }
+    if (currentFloor == "Loft")
+    {
+      if (offset == 0)
+      {
+        return "Loft";
+      }
+
+      if (offset == -1)
+      {
+        return "Floor03";
+      }
+    }
+
+    return "";
+  }
+  
+
+  [HarmonyPatch(typeof(InteractHandler), "OnUpdate")]
+  [HarmonyPrefix]
+  private static bool InteractTapper()
+  {
+    var isMapOpen = currentMap != "";
+    if (!doOnce && !upOnce && !downOnce && ((isMapOpen && (upButtonPressed || downButtonPressed)) || (!isMapOpen && customButtonPressed)))
+    {
+      _logger.LogInfo($"Interact tapped successfully {customButtonPressed} and {doOnce}");
+      doOnce = true;
+      _logger.LogInfo($"I have explicitly set doonce to fkin {doOnce}");
+      
+      var room = FuzzyGameState.LoreleiState.l_hCurrentLoreleiRoom;
+      if (room == null)
+      {
+        _logger.LogInfo("room mutherfucker null");
+        return true;
+      }
+      
+      var greatSuccess = room.mapLink.GetMapAndRoomInstance(
+        out FuzzyMap actualGodDamnMap, out FuzzyMap.Room alsoARoomButWhoCares);
+      if (!greatSuccess || actualGodDamnMap == null)
+      {
+        _logger.LogInfo("this mutherfucker null");
+        return true;
+      }
+      
+      var currentFloor = currentMap == "" ? actualGodDamnMap.name.Substring("pfHotelmap".Length) : currentMap;
+      var requestedFloor = currentFloor;
+      
+      _logger.LogInfo($"--------------");
+      _logger.LogInfo($"{room.name}");
+      _logger.LogInfo($"{actualGodDamnMap.name}");
+      StealMaptubesRefsIfAbsent();
+      
+      if (upButtonPressed && !upOnce)
+      {
+        upOnce = true;
+        requestedFloor = GetAdjacentFloorName(currentFloor, 1);
+      } else if (downButtonPressed && !downOnce)
+      {
+        downOnce = true;
+        requestedFloor = GetAdjacentFloorName(currentFloor, -1);
+      } else if (customButtonPressed && !doOnce)
+      {
+        doOnce = true;
+        requestedFloor = currentFloor;
+      }
+
+      if (requestedFloor == "")
+      {
+        _logger.LogInfo($"can't move further from {currentFloor}");
+        return true;
+      }
+      Interact maptubeInteract = Maptubes.Find(maptube => maptube.name.Contains(requestedFloor));
+      if (maptubeInteract == null)
+      {
+        _logger.LogInfo($"{currentFloor} not found");
+        return true;
+      }
+      
+      InteractHandler.Instance.ShowDialog(maptubeInteract, "", false);
+      currentMap = requestedFloor;
+      return false;
+    }
+
+    return true;
+  }
 
   [HarmonyPatch(typeof(PadLockLogic), ConfirmMethodName)]
   [HarmonyPatch(typeof(KeyCabinetPadLock), ConfirmMethodName)]
   [HarmonyPatch(typeof(LetterLockLogic), ConfirmMethodName)]
   [HarmonyPatch(typeof(RomanPadLockLogic), ConfirmMethodName)]
+  // [HarmonyPatch(typeof(MapTubeLogic), ConfirmMethodName)]
   [HarmonyPrefix]
   private static bool AlwaysPushConfirmButton(ref ConfirmButtonDuplicate ___m_hConfirmButton)
   {
@@ -42,6 +269,7 @@ public class Plugin : BaseUnityPlugin
   [HarmonyPatch(typeof(KeyCabinetPadLock), InputMethodName)]
   [HarmonyPatch(typeof(LetterLockLogic), InputMethodName)]
   [HarmonyPatch(typeof(RomanPadLockLogic), InputMethodName)]
+  // [HarmonyPatch(typeof(MapTubeLogic), InputMethodName)]
   [HarmonyPrefix]
   private static bool TreatVerticalDirectionAsInput(
     ref PadLockLogic __instance,
